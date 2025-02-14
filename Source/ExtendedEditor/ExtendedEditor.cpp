bdfb64b836201303bed32daa3f8ae18d66fabba7
Ilia Lukin <ilia64mods@gmail.com>
2025-02-14 19:01:23 +0300

    Init UE new project

---
+++ Source/ExtendedEditor/ExtendedEditor.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ExtendedEditor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ExtendedEditor, "ExtendedEditor" );
